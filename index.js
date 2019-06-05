c27bdcffc3c5a2677dcb982ec30d78ad9e7ff3e3
OussamaAlouat <al317257@uji.es>
2019-06-05 22:07:11 +0200

    Refactore code

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-function isPrimeNumber(number) {
-  if (number  === 1 || (number!== 2 && number%2 === 0)) {
-    console.log(`The number ${number} is not prime`);
-    return false;
-  }
-
-  let dividers = 0;
+function getDividers(number) {
   const arrayOfDividers = [];
   let index = 1;
   while (index <= number) {
     if (number % index === 0) {
-      dividers +=1;
       arrayOfDividers.push(index);
     }
 
     index++;
   }
 
-  if (dividers > 2) {
+  return arrayOfDividers;
+}
+
+function isPrimeNumber(number) {
+  if (number  === 1 || (number!== 2 && number%2 === 0)) {
     console.log(`The number ${number} is not prime`);
     return false;
   }
 
-  console.log(`The number ${number} is prime, its dividers are: ${arrayOfDividers}`)
+  const dividers = getDividers(number);
+  if (dividers.length > 2) {
+    console.log(`The number ${number} is not prime`);
+    return false;
+  }
+
+  console.log(`The number ${number} is prime, its dividers are: ${dividers}`)
 }
 
 isPrimeNumber(3);
