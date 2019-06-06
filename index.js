0925695f0ba79d3378bbea5ec31909320abaea04
OussamaAlouat <al317257@uji.es>
2019-06-06 07:21:49 +0200

    Adde an array of numbers to do tests

--- index.js
+++ index.js
@@ -17,11 +17,15 @@ function isPrimeNumber(number) {
 
   const dividers = getDividers(number);
   if (dividers.length > 2) {
-    console.log(`The number ${number} is not prime`);
+    console.log(`The number ${number} is not prime, dividers ${dividers}`);
     return false;
   }
 
   console.log(`The number ${number} is prime, its dividers are: ${dividers}`)
 }
 
-isPrimeNumber(3);
+const numbers = [2, 3, 5, 7, 11, 13, 20, 25, 33, 77, 99, 101, 999, 1029, 9999, 999999999];
+
+for(const number of numbers) {
+  isPrimeNumber(number);
+}
